623861a3ac4eb3744f036bbad881a7f71ab35b75
Jake Shepherd <jk.shepherd42@gmail.com>
2019-11-20 22:55:34 -0700

    major changes add color change, centered, grid reset

--- etch.js
+++ etch.js
@@ -1,19 +1,54 @@
 var gridpx = 960; // how many pixels 
 var gridsize = 16;
+var color = 'grey';
+var paint = true;
+document.getElementById("onOff").innerText = "ON";
 
+document.getElementById("numBlocks").addEventListener("input", resetGrid);
 
 displayGrid();
+enablePaint();
 
 function colorit(event){
-    event.target.style.backgroundColor = "grey";
+    if(paint == true)
+        event.target.style.backgroundColor = color;
 }
 
-rb = document.getElementById("restart");
-rb.addEventListener("click", function(){
-    console.log("Clicked");
-    gridsize = prompt("How many squares across?");
+function enablePaint(){
+    paint = true;
+    document.getElementById("onOff").innerText = "ON"; 
+    document.getElementById("onOff").style.color = "green";
+    document.getElementById("onOff").style.border = "green 2px solid";
+}
+
+function disablePaint(){
+    paint = false;
+    document.getElementById("onOff").style.border = "red 2px solid";
+    document.getElementById("onOff").style.color = "red";
+    document.getElementById("onOff").innerText = "OFF";
+}
+
+document.body.onkeyup = function(e){   
+    if(e.keyCode == 13){
+        //your code
+        if(paint == true){
+            disablePaint();
+            
+        }
+        else{
+            enablePaint();
+        }
+    }
+}
+
+function resetGrid(){
+    gridsize = document.getElementById("numBlocks").value;
     displayGrid();
-});
+
+}
+function setColor(picker) {
+    color = '#' + picker.toString()
+}
 
 function displayGrid(){
     document.getElementById("grid").innerHTML = "";
